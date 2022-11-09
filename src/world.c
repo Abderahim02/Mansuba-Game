2b6dfac4edf3959dfbfdff8fc8c01c56f8a7bd25
Finn Roters <finn.roters@enseirb-matmeca.fr>
2022-11-09 19:22:10 +0100

    test update for world.c

---
+++ src/world.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"world.h"
+
+// It's just a test if the upload to github works.
+struct world_t
+{
+    int position[WORLD_SIZE];
+};
+
