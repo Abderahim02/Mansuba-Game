d54e90723b63f3c9e1810364338c809017d87421
Abderahim Lagraoui <abderahim.lagraoui@bordeaux-inp.fr>
2022-11-15 16:06:31 +0100

    redefining world.c without modifying world.h

--- src/world.c
+++ src/world.c
@@ -1,62 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"world.h"
-
-// ____________________________________
-struct world_t
-{
-    // The available places in the Game.
-    int position[WORLD_SIZE-1];
-    // The two players of the Game.
-    enum current_player{Player1 = 1, Player2 = 2};
-    enum next_player{Playe1 = 1, Playe2 = 2};
-    // Maximum of allowed Moves.
-    int MAX_TURNS;
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "geometry.h"
+#include "world.h"
+struct world_t{ 
+  enum color_t colors[WORLD_SIZE];
+  enum sort_t sorts[WORLD_SIZE];
 };
 
-// ____________________________________
-struct world_t* world_init() 
-{
-    struct world_t world;
-    // Define maximum of turns.
-    world.MAX_TURNS = 2*WORLD_SIZE;
-    // No color, no sort everywhere in the begining.
-    for (int i = 0; i <= WORLD_SIZE; ++i) {
-        world.position[i] = 0;
-    }
+struct world_t* world_init(){
+  struct world_t world;
+  for(int i=0; i < WORLD_SIZE; ++i){ 
+      world.colors[i] = NO_COLOR;   
+      world.sorts[i] = NO_SORT;
+  }
+    return 0 ;
+}
+enum color_t world_get( const struct world_t* b, unsigned int idx){
+  enum color_t color = b->colors[idx];
+  return color;
+}
+// j'ai rencontré un problème lorsque je veux créer un monde du au fait que world_get prend un const struct world_t* 
+void world_set(struct world_t* b, unsigned int idx, enum color_t c){
+  b->colors[idx] = c;
+}
+enum sort_t world_get_sort(const struct world_t* b, unsigned int idx){
+  enum sort_t sort = b->sorts[idx];
+  return sort;
+}
+void world_set_sort(struct world_t* b, unsigned int idx, enum sort_t c){
+  b->sorts[idx] = c;
 }
 
-/*
-// ___________________________________
-enum color_t world_get(const struct world_t* b, unsigned int idx)
-{
-
-}
-
-// ___________________________________
-void world_set(struct world_t* b, unsigned int idx, enum color_t c)
-{
-
-}
-
-// ____________________________________
-enum sort_t world_get_sort(const struct world_t* b, unsigned int idx)
-{
-
-}
-
-//_____________________________________
-void world_set_sort(struct world_t* b, unsigned int idx, enum sort_t c)
-{
-
-}
-*/
-
-// The main function, mainly for testing.
-int main(int argc, char *argv[])
-{
-    printf("world.c is compiling.\n"); // Test for Makefile
+int main(int argc, char *argv[]){
     return 0;
-}
+    }
