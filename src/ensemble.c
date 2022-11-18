3b5a73eaf619da5917096d6fcbca1c0a5ca1a291
Finn Roters <finn.roters@enseirb-matmeca.fr>
2022-11-18 17:59:39 +0100

    Modified ensemble.h and ensemble.c

--- src/ensemble.c
+++ src/ensemble.c
@@ -5,51 +5,36 @@
 #include "geometry.h"
 #include "neighbors.h"
 
-// _____________________
-enum players{
-  PLAYER_WHITE = 0,
-  PLAYER_BLACK = 1,
-};
-struct position_info {
-    int initial[HEIGHT];
-    int allowed_moves[HEIGHT];
-    int current_pieces[HEIGHT];
-};
 
-struct player {
-  struct position_info position_info;
-  enum players player;
+struct position_info {
+    int initial_White[HEIGHT];
+    int initial_Black[HEIGHT];
+    int allowed_moves_White[HEIGHT];
+    int allowed_moves_Black[HEIGHT];
+    int current_pieces_White[HEIGHT];
+    int current_pieces_Black[HEIGHT];
 };
 
 // _________________________
-<<<<<<< HEAD
-void init_players(struct world_t* worldt) {
-  for (int i = 0; i < WORLD_SIZE; ++i) {
-    // In the begin we put the white pieces to the left and the black pieces to the right.
-    if (i % WIDTH == 0) {
-      worldt->colors[i] = WHITE;
-      worldt->sorts[i] = 1;
-=======
 void init_players(struct world_t* b) {
-    // struct world_t *world = world_init();
+    struct position_info position_info;
     for (int i = 0; i <= WORLD_SIZE; ++i) {
         // In the begin we put the white pieces to the left and the black pieces to the right.
         if (i % WIDTH == 0) {
             world_set(b,i,WHITE);
             world_set_sort(b,i,1);
+            position_info.initial_White[i] = i;
+            position_info.current_pieces_White[i] = i;
         }
         else if (i % WIDTH-1 == 0) {
             world_set(b,i,BLACK);
-            world_set_sort(b,i,1);       
+            world_set_sort(b,i,1);
+            position_info.initial_Black[i] = i;
+            position_info.current_pieces_Black[i] = i;
         }
->>>>>>> origin/master
     }
-    else if (i % WIDTH-1 == 0) {
-      worldt->colors[i] = BLACK;
-      worldt->sorts[i] = 1;
-    }
-  }
 }
+
 int is_newex_neighbor(int ex_idx,int new_idx){
   for (int i=0; i < MAX_NEIGHBORS; ++i){
     if(nei
