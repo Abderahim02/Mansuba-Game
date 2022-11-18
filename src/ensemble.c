2aa7ffe2f396fe1fc2e2c1331171ed7fc8eead49
Abderahim Lagraoui <abderahim.lagraoui@bordeaux-inp.fr>
2022-11-18 17:08:07 +0100

    added some manipulating functions of the playing boeard

--- src/ensemble.c
+++ src/ensemble.c
@@ -6,6 +6,10 @@
 #include "neighbors.h"
 
 // _____________________
+enum players{
+  PLAYER_WHITE = 0,
+  PLAYER_BLACK = 1,
+};
 struct position_info {
     int initial[HEIGHT];
     int allowed_moves[HEIGHT];
@@ -13,30 +17,41 @@ struct position_info {
 };
 
 struct player {
-    struct position_info position_info;
-
-};
-
-struct players {
-    struct player player1;
-    struct player player2;
+  struct position_info position_info;
+  enum players player;
 };
 
 // _________________________
-void init_players(struct world_t* world) {
-    struct world_t *world = world_init();
-    for (int i = 0; i <= WORLD_SIZE; ++i) {
-        // In the begin we put the white pieces to the left and the black pieces to the right.
-        if (i % WIDTH == 0) {
-            world->colors[i] = WHITE;
-            world->sorts[i] = 1;
-        }
-        else if (i % WIDTH-1 == 0) {
-            world->colors[i] = BLACK;
-            world->sorts[i] = 1;
-        }
+void init_players(struct world_t* worldt) {
+  for (int i = 0; i < WORLD_SIZE; ++i) {
+    // In the begin we put the white pieces to the left and the black pieces to the right.
+    if (i % WIDTH == 0) {
+      worldt->colors[i] = WHITE;
+      worldt->sorts[i] = 1;
     }
+    else if (i % WIDTH-1 == 0) {
+      worldt->colors[i] = BLACK;
+      worldt->sorts[i] = 1;
+    }
+  }
+}
+int is_newex_neighbor(int ex_idx,int new_idx){
+  for (int i=0; i < MAX_NEIGHBORS; ++i){
+    if(nei
+  }
+}
 
+  
+int is_allowed_tomove(int ex_idx,int new_idx){
+  if () 
+}
+
+void move_player(struct world_t world, struct player player, int ex_idx, int new_idx ){
+  switch (player.player){
+  case PLAYER_WHITE :
+    world.colors[new_idx] = world.colors[ex_idx];
+    player.current[new_id]=player.current[ex_id]; 
+  }
 }
 
 
