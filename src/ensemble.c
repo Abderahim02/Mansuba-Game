037da983d0952914e5c0b04f31ec39a30d738c0f
Abderahim Lagraoui <abderahim.lagraoui@bordeaux-inp.fr>
2022-11-22 23:52:28 +0100

    fixed infinite loop problem

--- src/ensemble.c
+++ src/ensemble.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ensemble.h"
-/*
 #include "world.h"
 #include "geometry.h"
 #include "neighbors.h"
-*/
 
 // _____________________
 struct world_t{ 
@@ -20,24 +17,17 @@ enum players{
   PLAYER_BLACK = 1 ,
 };
 struct positions_info { 
-  int initial_WHITE[HEIGHT];
-  /*  à ce pont on  a pas besion de definir le champ mouvements possibles pour chaque joueur */
-  int current_pieces_WHITE[HEIGHT]; // les positions des pièces du joieur avec les pieces blahces
-  int initial_BLACK[HEIGHT];
-  int current_pieces_BLACK[HEIGHT]; //les positions des pièces du joueur avec les pieces blahces
+  /*  at this poin we don't have to define a structure for possible mouvements of each player */
+  int initial_WHITE[HEIGHT]; // initial positions of he player with white pawns
+  int current_pieces_WHITE[HEIGHT]; // current positions of the player with white pawns
+  int initial_BLACK[HEIGHT];  // initial positions of he player with black pawns
+  int current_pieces_BLACK[HEIGHT]; //current positions of the player with black pawns
 };
 
+//____________________________________________________________________________________________
 
-// _____________________cette fonction initialise le monde avec les positions initales de chaque joueur , on suppose que le joueur avec les pieces blanches occupe les positions latèrales à gauche tants que l'autre joueur occupe celles à droite 
-
-// ____________
-/*void add_to_current_piece(struct positions_info positions_info, unsigned int ex_idx, unsigned int new_idx){
-  for(int i=0 ; i < HEIGHT; ++i ){
-    if (positions_info)
-  }
-  }
-}____*/
-//cette fonction initialise les informations sur les joueurs NB: les tableaux sont remplis initialement par WORLD_SIZE partout)
+/*this function initialize informations at the begining of the game , we initialize the current pieces with WORLD_SIZE everywere , 
+this will be useful for the function add_to_current_piece */
 struct positions_info init_infos(){
   struct positions_info infos;
   for (int i=0; i < HEIGHT ; ++i){
@@ -46,6 +36,7 @@ struct positions_info init_infos(){
   }
   return infos;
 }
+/* this function add the position idx to current pieces for the player , we will need it just at the begining of the game */
 void add_to_current_piece(struct positions_info positions_info, enum players player, unsigned int idx){
   switch (player){
   case PLAYER_WHITE:
@@ -68,6 +59,8 @@ void add_to_current_piece(struct positions_info positions_info, enum players pla
         break;
   }
 }
+/* it's the function that give for each player his initial positions , we suppose the initially the player with white pawns 
+will take the last column at the left edge and the other player will take the last column at the right edge */
 void init_players(struct world_t* b, struct positions_info positions_info) {
     //struct positions_info positions_info;
     for (int i = 0; i < WORLD_SIZE; ++i) {
@@ -76,24 +69,20 @@ void init_players(struct world_t* b, struct positions_info positions_info) {
             world_set(b,i,WHITE);
             world_set_sort(b,i,1);
             add_to_current_piece(positions_info, PLAYER_WHITE, i);
-            //positions_info.initial_WHITE[i] = 1;
-            //positions_info.current_pieces_WHITE[i] = 1;
         }
         else if ((i% WIDTH)  == WIDTH - 1 ) {
             world_set(b, i, BLACK);
             world_set_sort(b, i, 1);
             add_to_current_piece(positions_info, PLAYER_BLACK, i);
-            //positions_info.initial_BLACK[i] = 1;
-            //positions_info.current_pieces_BLACK[i] = 1;
         }
     }
 }
 
-//____________ensemble de fonctions auxiliares :
-//la premiere sert à verifier si la case à laquelle on veut se deplacer new_idx est un voisin de la case ex_idx:
+//_____________________________a list of functions that will help us for the move_player function :
+/*this one verify if new_idx is a neighbor of ex_idx*/
 
 int is_new_ex_neighbor(unsigned int ex_idx, unsigned int new_idx){
-  struct neighbors_t neighbors = get_neighbors(ex_idx);
+  struct neighbors_t neighbors = get_neighbors(ex_idx); //we use get_neighbor to get the neighbors list of ex_idx 
   for ( int j=0; j < MAX_NEIGHBORS; ++j){
     if(neighbors.n[j].i == new_idx){
       return 1;
@@ -101,19 +90,31 @@ int is_new_ex_neighbor(unsigned int ex_idx, unsigned int new_idx){
   }
   return 0;
 }
-//cette fct sert à verifier si on a le droit de se deplacer vers cette case:
+/*this one verify if it is an allowed simple move */
   
-int is_allowed_to_move(struct world_t* world, unsigned int ex_idx, unsigned int new_idx){
-  if (is_new_ex_neighbor(ex_idx, new_idx)){ // si new_idx est un voisin 
-    if( world->sorts[new_idx] == 0){
+int is_allowed_to_simple_move(struct world_t* world, unsigned int ex_idx, unsigned int new_idx){
+  if (is_new_ex_neighbor(ex_idx, new_idx)){ // we check if new_ex is a neighbor 
+    if( world->sorts[new_idx] == 0){ //we check if new_ex is a free position
       return 1;
     }
-  }
-  //a ce stade on effectue que des mouvements simples
+   }
+
+  //at the moment we do just simple moves
   return 0;
 }
+/*int allowed_simple_jump(struct world_t* world, unsigned int ex_idx, unsigned int new_idx){
+  if (is_new_ex_neighbor(ex_idx, new_idx) == 0){
+    struct neighbors_t neighbors = get_neghbors(ex_idx);
+    int j=0;
+    while( j < MAX_NEGHBORS && world->sorts[neighbors.n[j].i] != 0){  we see all existants neighbors of ex_idx 
+      return is_allowed_to_simple_move(world, neighbors.n[j].i, idx);   when the neighbor exist we see if we can do a simple move from the newx_idx neghbor's 
+      position to its  neighbors positions , it means we see if a neighbor 
+    }
+  }
+  return 0;
+}*/
 
-//une fonction qui met a jour la liste des positions des pieces des joueurs
+//this function update player's information after every move 
 
 void update_current_pieces(enum players player, struct positions_info infos, unsigned int ex_idx, unsigned int new_idx){
   if( player == 1){
@@ -125,36 +126,32 @@ void update_current_pieces(enum players player, struct positions_info infos, uns
   }
   else{
     for(int i=0; i < HEIGHT; ++i){
-      if( infos.current_pieces_WHITE[i] == ex_idx ){
+      if( infos.current_pieces_WHITE[i] == ex_idx ){ //we replace the position ex_idx it new_idx
 	        infos.current_pieces_WHITE[i] = new_idx;
       }
     }
   }
 }
-// c'est la fonction qui fait un mouvement d'une piece si c'est possible
+// this is our function that do the move if it is allowed
 
 void move_player(struct world_t* world, enum players player, struct positions_info infos, unsigned int ex_idx, unsigned int new_idx ){
   switch (player){
-  case PLAYER_BLACK : //player_black_pawns
-    if(is_allowed_to_move(world, ex_idx, new_idx)){
+  case PLAYER_BLACK : //player with black_pawns
+    if(is_allowed_to_simple_move(world, ex_idx, new_idx) == 1){
       world->colors[new_idx] = BLACK;
       world->colors[ex_idx] = NO_COLOR ;
       world->sorts[ex_idx] = NO_SORT;
       world->sorts[new_idx] = PAWN;
       update_current_pieces(player, infos, ex_idx, new_idx);
-      //infos.current_pieces_BLACK[new_id] = infos.current_pieces_BLACK[ex_id]; 
     }
     break;
-  case PLAYER_WHITE: //player_white_pawn
-    if(is_allowed_to_move(world, ex_idx, new_idx)){
-      world->colors[new_idx] = BLACK;
+  case PLAYER_WHITE: //player with white_pawn
+    if(is_allowed_to_simple_move(world, ex_idx, new_idx) == 1){
+      world->colors[new_idx] = WHITE;
       world->colors[ex_idx] = NO_COLOR ;
       world->sorts[ex_idx] = NO_SORT;
       world->sorts[new_idx] = PAWN;
       update_current_pieces(player, infos, ex_idx, new_idx);
-
-      
-      //infos.current_pieces_BLACK[new_id] = infos.current_pieces_BLACK[ex_id]; 
     }
     break;
   default:
@@ -173,6 +170,10 @@ void print_world( struct world_t* world ){
   }
 }
 
+
+// ________________________a test with two rounds game
+
+ 
 int main() {
   struct world_t* world = world_init();
   struct positions_info positions = init_infos();
@@ -180,7 +181,8 @@ int main() {
   printf("First round: \n");
   print_world(world);
   printf("\n");
-  move_player(world, PLAYER_WHITE, positions, 0, 2);
+  
+  move_player(world, PLAYER_WHITE, positions, 0, 1);
   printf("Second round:\n");
   print_world(world);
   printf("\n");
