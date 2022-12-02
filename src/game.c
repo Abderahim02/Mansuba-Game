4a49c728946adacdbe22128fe7aa56fa6889de57
Finn Roters <finn.roters@enseirb-matmeca.fr>
2022-12-02 15:23:58 +0100

    Added multi jump in game function

--- src/game.c
+++ src/game.c
@@ -27,7 +27,7 @@ enum players current_player = PLAYER_WHITE;
 
 // function for getting a random player
 enum players get_random_player() {
-    srand(time(0));
+    // srand(time(0));
     // % will help us to get either 0 or 1.
     int rand_player = (rand() % 2) + 1;
     return rand_player;
@@ -64,30 +64,38 @@ int nobody_has_won(struct world_t* world, struct positions_info infos){
 }
 
 
-struct move choose_random_move_for_piece(struct world_t* world, struct positions_info infos, unsigned int ex_idx){
+struct move choose_random_move_for_piece(struct world_t* world, enum players player, struct positions_info infos, unsigned int ex_idx){
     struct move move; 
     move.ex_idx = ex_idx;
     unsigned int i=0;
-    while(i < WORLD_SIZE){
+    while(is_allowed_simple_jump(world, ex_idx, i)==0 && is_allowed_to_simple_move(world, ex_idx, i) == 0){
+        i = rand() % WORLD_SIZE;
+        if (is_multi_jump_allowed(world, player, infos, ex_idx)) {
+            move.type = MULTIPLE_JUMP;
+            return move;
+        }
         if(is_allowed_simple_jump(world, ex_idx, i)){
-        move.new_idx = i;
-        move.type = SIMPLE_JUMP;
-        return move;
+            move.new_idx = i;
+            move.type = SIMPLE_JUMP;
+            return move;
         }
         else if (is_allowed_to_simple_move(world, ex_idx, i)){
-        move.new_idx = i;
-        move.type = SIMPLE_MOVE;
-        return move;
+            move.new_idx = i;
+            move.type = SIMPLE_MOVE;
+            return move;
         }
-        else{
-        ++i;
-        }
+        
     } 
     return move;
 }
 
 void move_current_player(struct world_t* world, enum players player, struct positions_info infos, struct move move){
     switch (move.type){
+        case MULTIPLE_JUMP:
+            // multi_jump(world, player, infos, move.ex_idx);
+            printf("I did a multiple JUMP!! \n");
+            move.new_idx = multi_jump(world, player, infos, move.ex_idx);
+            break;
         case SIMPLE_JUMP:
             simple_jump(world, player, infos, move.ex_idx, move.new_idx);
             update_current_pieces(player, &infos, move.ex_idx, move.new_idx);
@@ -107,32 +115,24 @@ void move_current_player(struct world_t* world, enum players player, struct posi
 int main(){
     struct world_t* world = world_init();
     struct positions_info infos;
+    srand(time(0));
     init_infos_2(&infos);
     init_players(world, infos);
     enum players current_player = get_random_player();
     while(nobody_has_won(world, infos)){
         unsigned int p = choose_random_piece_belonging_to(infos, current_player);
-        struct move random_move = choose_random_move_for_piece(world, infos, p);
+        struct move random_move = choose_random_move_for_piece(world, current_player, infos, p);
         move_current_player( world, current_player, infos, random_move);
         print_world(world);
         printf("\n");
-        //printf("random position: %d\n", choose_random_piece_belonging_to(infos, current_player));
-        //move_current_player(world, current_player, infos, random_move);
-        //print_world(world);
-        //current_player = next_player(current_player);
         //printf("Current player:%d Next player %d\n", current_player, next_player(current_player));
         //printf("Choose random peace: %d belonging to: %d\n",choose_random_piece_belonging_to(infos, current_player), current_player);
-        print_current_pieces(infos);
-        usleep(750 * 100);
-        // printf("Choose random peace: %d belonging to: %d\n", p , current_player);
         update_current_pieces(current_player, &infos, random_move.ex_idx, random_move.new_idx);
-        printf("Moving from %d to %d and using move %d \n", random_move.ex_idx, random_move.new_idx, random_move.type);
+        printf("_______________________________________\n");
         print_current_pieces(infos);
-        move_current_player(world, current_player, infos, random_move);
-        print_world(world);
         printf("\n");
         current_player = next_player(current_player);
-        // usleep(8500 * 1000);
+        usleep(500 * 1000);
     }
     return 0;
 }
