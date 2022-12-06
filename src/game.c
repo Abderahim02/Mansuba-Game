45c3fcbd795acd4233a29183fe8e05aa9b462e91
Finn Roters <finn.roters@enseirb-matmeca.fr>
2022-12-06 15:20:00 +0100

    Fixed infinity moves

--- src/game.c
+++ src/game.c
@@ -59,7 +59,9 @@ unsigned int choose_random_piece_belonging_to(struct positions_info infos, unsig
     default:
         break;
     }
+    return 0;
 }
+
 //a function that test if there is no winner 
 int nobody_has_won(struct world_t* world, struct positions_info infos){
     return (simple_win(world, PLAYER_WHITE, infos) || simple_win(world, PLAYER_BLACK, infos))==0;
@@ -69,7 +71,7 @@ int nobody_has_won(struct world_t* world, struct positions_info infos){
 struct move choose_random_move_for_piece(struct world_t* world, enum players player, struct positions_info infos, unsigned int ex_idx){
     struct move move; 
     move.ex_idx = ex_idx;
-    unsigned int i = 92;
+    unsigned int i = 45;
     while(is_allowed_simple_jump(world, ex_idx, i)==0 && is_allowed_to_simple_move(world, player, infos, ex_idx, i) == 0 
         && is_multi_jump_allowed(world, player, infos, ex_idx) == 0){
         i = rand() % WORLD_SIZE;
@@ -91,6 +93,7 @@ struct move choose_random_move_for_piece(struct world_t* world, enum players pla
     } 
     return move;
 }
+
 //_________________simple moves only test______
 struct move choose_random_simple_move_for_piece(struct world_t* world, enum players player, struct positions_info infos, unsigned int ex_idx){
     struct move move; 
@@ -129,10 +132,12 @@ void move_current_player(struct world_t* world, enum players player, struct posi
                 break;
             case SIMPLE_JUMP:
                 simple_jump(world, player, infos, move.ex_idx, move.new_idx);
+                // printf("I did a simple JUMP!! \n");
                 //update_current_pieces(current_player, &infos, move.ex_idx, move.new_idx);
                 break;
             case SIMPLE_MOVE:
                 simple_move_player(world, player, infos, move.ex_idx, move.new_idx);
+                // printf("I did a simple MOVE!! \n");
                 //update_current_pieces(current_player, &infos, move.ex_idx, move.new_idx);
                 break;
             default:
@@ -156,17 +161,18 @@ int main(){
     enum players current_player = get_random_player();
     unsigned int p = choose_random_piece_belonging_to(infos, current_player);
     while(nobody_has_won(world, infos) /*&& infos.TURNS < 100*/){
-        unsigned int p = choose_random_piece_belonging_to(infos, current_player);
+        p = choose_random_piece_belonging_to(infos, current_player);
         struct move random_move = choose_random_move_for_piece(world, current_player, infos, p);
+        // move_current_player( world, current_player, infos, random_move);
+        // if(is_allowed_to_simple_move(world, current_player, infos, random_move.ex_idx, random_move.new_idx)) {
         move_current_player( world, current_player, infos, random_move);
-       // if(is_allowed_to_simple_move(world, current_player, infos, random_move.ex_idx, random_move.new_idx)){
         //simple_move_current_player( world, current_player, infos, random_move);
         update_current_pieces(current_player, &infos, random_move.ex_idx, random_move.new_idx);
         //print_current_pieces(infos);
         print_world(world);
         printf("\n");
         printf("_____________________________\n");
-       // }
+        // }
         if(simple_win(world, current_player, infos)){
             char *victor = (current_player == PLAYER_WHITE)? "PLAYER_WHITE" : "PLAYER_BLACK" ;
             printf("the winner is : %s\n", victor);
